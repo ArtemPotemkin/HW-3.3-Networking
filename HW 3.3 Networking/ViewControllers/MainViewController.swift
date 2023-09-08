603b458900b49168bda7ea7acbac7eec8c79e074
Artem Potemkin <art.potemkin@gmail.com>
2023-09-08 16:06:39 +0300

    Networking by first variant

--- HW 3.3 Networking/ViewControllers/MainViewController.swift
+++ HW 3.3 Networking/ViewControllers/MainViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 final class MainViewController: UITableViewController {
     
-    let emojis = EmojiMok.getEmoji()
+    let emojisMok = EmojiMok.getEmoji()
+    var emojis: [Emoji] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 70
+        fetchEmojis()
 
     }
 
@@ -32,51 +34,29 @@ final class MainViewController: UITableViewController {
         
         return cell
     }
-   
+}
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+
+// MARK: - Networking
+extension MainViewController {
+    func fetchEmojis() {
+        guard let url = URL(string: "https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json") else { return }
+        
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                self?.emojis = try decoder.decode([Emoji].self, from: data)
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

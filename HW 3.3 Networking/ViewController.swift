ede9912e814cff94633214d4b95e48382d08b923
Artem Potemkin <art.potemkin@gmail.com>
2023-09-08 14:03:29 +0300

    Initial Commit

---
+++ HW 3.3 Networking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 3.3 Networking
+//
+//  Created by Артём Потёмкин on 08.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
